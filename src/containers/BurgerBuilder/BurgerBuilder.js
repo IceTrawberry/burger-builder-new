e0239e4856a5556343687b0d7286437e5f0592eb
arya <zyu252@wisc.edu>
2019-09-02 18:34:57 -0500

    Created an order summary modal
    Make sure the key is unique.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import Burger from '../../components/Burger/Burger'
 import Aux from '../../hoc/Aux';
 import React, {Component} from 'react';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 1,
@@ -71,6 +73,7 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = Boolean(disabledInfo[key] <= 0)
     }
     return (<Aux>
+      <Modal><OrderSummary ingredients={this.state.ingredients}/></Modal>
       <Burger ingredients={this.state.ingredients}/>
       <BuildControls ingredientAdded={this.addIngredientHandler}
         ingredientRemoved={this.removeIngredientHandler}
